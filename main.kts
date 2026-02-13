7a0223c58d9e9dbb226a78bc2b1aa3d516819bec
Digao0 <rodrigomartinsdearaujo@hotmail.com>
2026-02-12 22:40:09 -0300

    logica usando listas implementada

--- main.kts
+++ main.kts
@@ -1,13 +1,64 @@
 //println("hello kotlin")
 
 //variaveis
+//var inicio = true
+//var flag_space = false
+//var current_op = '+'
 val equacao = args[0]
-var inicio = true
 var soma = 0
 var numero = ""
-var cont = 0
-var flag_space = false
 
+
+var ops_validas = listOf('+', '-')
+var ops = mutableListOf<Char>()
+var nu = mutableListOf<Int>()
+
+if (equacao.isEmpty()) {
+    throw Exception("Entrada invalida") //checa string vazia
+}
+
+var cont = 0
+while (cont < equacao.length){
+    var char = equacao[cont]
+
+    if (char.isDigit()){
+
+        while (char.isDigit() == true){
+            char = equacao[cont]
+            numero += char 
+            cont++   
+        }
+
+        nu.add(numero.toInt())
+        numero = ""
+    } else if (char in ops_validas){
+        ops.add(char)
+        cont++
+    } else if (char == ' '){
+        cont++
+    } else {
+        throw Exception("Entrada invalida")
+    }
+
+}
+
+if (nu.size != ops.size + 1){
+    throw Exception("Entrada invalida") //checa se tem numero a mais ou operador a mais
+}
+
+soma = nu[0]
+for (i in ops.indices){
+    if (ops[i] == '+'){
+        soma += nu[i + 1]
+    } else if (ops[i] == '-'){
+        soma -= nu[i + 1]
+    }
+}
+
+println(soma)
+
+
+/* 
 while (cont < equacao.length) {
     val char = equacao[cont]
 
@@ -41,8 +92,7 @@ while (cont < equacao.length) {
         } else {
             cont++
             continue
-        }
-        
+        }    
     } else {
         throw Exception("Entrada invalida")
     }
@@ -61,7 +111,7 @@ while (cont < equacao.length) {
     cont ++
 }
     
-println(soma)
+println(soma) */
 
 
 /* 
